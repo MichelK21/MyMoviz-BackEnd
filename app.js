08c3a6a3415fb0d5ac318adfc1541814de482df0
Michel KACOU <Your email>
2023-02-01 16:36:21 +0100

    maj

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var indexRouter = require("./routes/index");
 
-require(dotenv).config();
+require("dotenv").config();
 
 var app = express();
 
